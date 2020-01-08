152ea2eafbf79fecdae69f1069e9dc66c9bbfeb1
Falk Hoppe <falkhoppe81@gmail.com>
2020-01-08 10:38:07 +0100

    fix priority of link-rel vs resource name for linking

--- client/js/helpers/path-item-helper.js
+++ client/js/helpers/path-item-helper.js
@@ -2,7 +2,7 @@
 import { slugify } from '../../../vendor/transliteration/dist/browser/bundle.esm.min.js'
 
 export const pathID = (path, pathItem) => {
-  const id = pathItem && (pathItem['x-resource-name'] || pathItem['x-link-rel'])
+  const id = pathItem && (pathItem['x-link-rel'] || pathItem['x-resource-name']) // Link-Rel over Link-Name over path
   return id || path
 }
 
